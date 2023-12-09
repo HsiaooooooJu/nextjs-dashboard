84837c5bd041f87c15979c53d83eba79b42ab918
hsiaoju <hsiaojuko@liontravel.com>
2023-12-09 15:51:57 +0800

    feat: add responsive image

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/paint.jpg"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          ></Image>
+          <Image
+            src="/paint.jpg"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          ></Image>
         </div>
       </div>
     </main>
